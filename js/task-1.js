7400a38ff2dbbf49fffeecb42c51b8134a88e4cd
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-03-28 21:43:22 +0200

    task-1: fix category count and items count

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,11 @@
 (function () {
   const categories = document.querySelector('#categories');
-  const countOfCategories = categories?.querySelectorAll('h2').length;
-  console.log(`Number of categories: ${countOfCategories}`);
+  const categoryCount = categories?.querySelectorAll('li.item').length;
+  console.log(`Number of categories: ${categoryCount}`);
   categories?.childNodes.forEach(item => {
     if (item.nodeName === 'LI') {
       const title = item.querySelector('h2').textContent;
-      const countOfItems = item.querySelectorAll('ul>li').length;
+      const countOfItems = item.querySelectorAll('li').length;
       console.log(`Category: ${title}`);
       console.log(`Elements: ${countOfItems}`);
     }
